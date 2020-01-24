3a2ac2e0c3043e36b0a0b987667f3024e177518f
AJ Keeter <aleckeeter94@gmail.com>
2020-01-24 15:01:15 -0600

    Implemented edit and clear functionality to contacts and contacts form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
                 phone: "111-111-1111",
                 type: "professional"
             }
-        ]
+        ],
+        current: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -52,9 +53,13 @@ const ContactState = props => {
         dispatch({ type: DELETE_CONTACT, payload: id });
     };
     // Set Current Contact
-
+    const setCurrent = contact => {
+        dispatch({ type: SET_CURRENT, payload: contact });
+    };
     // Clear Current Contact
-
+    const clearCurrent = () => {
+        dispatch({ type: CLEAR_CURRENT });
+    };
     //Update Contact
 
     // Filter Contacts
@@ -65,8 +70,11 @@ const ContactState = props => {
         <ContactContext.Provider
             value={{
                 contacts: state.contacts,
+                current: state.current,
                 addContact,
-                deleteContact
+                deleteContact,
+                setCurrent,
+                clearCurrent
         }}>
             { props.children }
         </ContactContext.Provider>
