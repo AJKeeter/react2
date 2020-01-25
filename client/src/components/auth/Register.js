2d381178e7ac69b695556aaeef54965eb664dc09
AJ Keeter <aleckeeter94@gmail.com>
2020-01-25 12:11:07 -0600

    imports AlertContext into the register file and creates if statement, no issues for site.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, {useContext, useState} from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Register = () => {
+    const alertContext = useContext(AlertContext);
+
+    const { setAlert } = alertContext;
+
     const [user, setUser] = useState({
         name: '',
         email: '',
         password: '',
-        password2: '',
+        password2: ''
     });
 
     const { name, email, password, password2 } = user;
@@ -14,7 +19,13 @@ const Register = () => {
 
     const onSubmit = e => {
         e.preventDefault();
-        console.log('Register submit')
+        if(name === '' || email === '' || password === '') {
+            setAlert('Please enter all fields', 'danger');
+        } else if (password !== password2) {
+            setAlert('Passwords do not match', 'danger');
+        } else {
+            console.log('Register submit')
+        }
     };
 
     return (
